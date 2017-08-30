b262257915bbd6aece8ac8a4c6e6710a222280e9
Aurora Hager <roryy18@gmail.com>
2017-08-29 20:38:27 -0500

    Edabit

--- scrpit.js
+++ scrpit.js
@@ -1,1 +1,36 @@
 console.log('js');
+
+// EDABIT PRLBM: Is The Number Even Or Odd?
+
+function isEvenOrOdd(num) {
+    // is the number even?
+    if (num % 2 === 0) {
+        return "even";
+    } // END if even 
+    else {
+        return "odd";
+    } // END if odd 
+} // END isEvenOrOdd function
+
+/*''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''*/
+
+//EDABIT PRLBM: Is The Number Even Or Odd?
+
+// declare globals
+var myNum = 2;
+var myDec = 0.44;
+
+// function that checks if a number is < or = 0
+function lessThanOrEqualToZero(num) {
+    // if num
+    if (num <= 0) {
+        return true;
+    }//END if num
+    else {
+        return false;
+    }//END else 	
+}//END lessThanOrEqualToZero function
+
+// CALL myNum and myDec into lessThanOrEqualToZero
+lessThanOrEqualToZero(myNum);
+lessThanOrEqualToZero(myDec);
